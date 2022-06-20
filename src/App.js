2e18f500aa0f37749ef47fd27b615a3854d4caf3
alan-serrano <alan.serrano.pch@gmail.com>
2022-06-20 17:24:54 -0400

    feat/deliverable1 base homePage view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
+import { GlobalStyle } from './styles';
 import { useFeaturedBanners } from './utils/hooks/useFeaturedBanners';
+import HomePage from './views/HomePage';
 
 function App() {
   const { data, isLoading } = useFeaturedBanners();
@@ -8,20 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomePage />
     </div>
   );
 }
---
+++ src/views/HomePage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { FullLayout } from '../layout';
+
+const HomePage = () => {
+  return <FullLayout>Hello world</FullLayout>;
+};
+
+export default HomePage;
