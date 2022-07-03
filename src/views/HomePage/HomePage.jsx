8478858bd2700c61e991dc6017209b215f4af9cf
alan-serrano <alan.serrano.pch@gmail.com>
2022-07-03 16:59:09 -0400

    feat/deliverable1 Adding sections on homePage

--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import CategoryGrid from '../../components/CategoryGrid/CategoryGrid';
 import FeaturedProductGrid from '../../components/FeaturedProductGrid/FeaturedProductGrid';
+import { FeaturedBannerSlider as FeaturedBannerSliderStyled } from '../../components/FeaturedBannerSlider/styles';
+import { CategorySlider as CategorySliderStyled } from '../../components/CategorySlider/styles';
 import { FullLayout } from '../../layout';
 
-import categories from '../../mocks/es-mx/product-categories.json';
-import featuredProducts from '../../mocks/es-mx/featured-products.json';
+import categories from '../../mocks/en-us/product-categories.json';
+import featuredProducts from '../../mocks/en-us/featured-products.json';
+import featuredBannerInfo from '../../mocks/en-us/featured-banners.json';
 
 const HomePage = () => {
   return (
     <FullLayout>
+      <section className="featured-banners container">
+        <h2>Featured Banners</h2>
+        <FeaturedBannerSliderStyled
+          featuredBanners={featuredBannerInfo.results}
+          slidesToShow={1}
+        />
+      </section>
       <section className="categories container">
-        <CategoryGrid categories={categories.results} gap={2} />
+        <h2>Product Categories Slider</h2>
+        <CategorySliderStyled
+          categories={categories.results}
+          slidesToShow={3}
+        />
       </section>
 
       <section className="featured-products container">
+        <h2>Featured Products Grid</h2>
         <FeaturedProductGrid
           featuredProducts={featuredProducts.results}
           gap={2}
