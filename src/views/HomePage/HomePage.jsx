e9b3a3d8b36511a9228899857630b76ceb1fff44
alan-serrano <alan.serrano.pch@gmail.com>
2022-07-04 13:53:03 -0400

    feat/deliverable1 fix responsive slider

--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -35,7 +35,7 @@ const HomePage = (props) => {
           <h2>Product Categories Slider</h2>
           <CategorySliderStyled
             categories={categories.results}
-            slidesToShow={breakpoint !== 'smartPhonePortrait' ? 2 : 3}
+            slidesToShow={breakpoint === 'smartPhone' ? 2 : 3}
           />
         </section>
         <section className="featured-products container">
