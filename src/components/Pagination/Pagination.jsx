4205517c2d6e5bee7ba4f38677a103b63e8acca2
alan-serrano <alan.serrano.pch@gmail.com>
2022-07-04 07:49:24 -0400

    feat/deliverable2 creating pagination

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Pagination = ({ className, count = 5 }) => {
+  return (
+    <nav>
+      <ul className={className}>
+        <li className="pagination-item">
+          <div className="arrow left" />
+        </li>
+
+        {Array.from({ length: count }).map((value, i) => (
+          <li className="pagination-item">{i + 1}</li>
+        ))}
+
+        <li className="pagination-item">
+          <div className="arrow right" />
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+Pagination.propTypes = {
+  count: PropTypes.number.isRequired,
+};
+
+export default Pagination;
