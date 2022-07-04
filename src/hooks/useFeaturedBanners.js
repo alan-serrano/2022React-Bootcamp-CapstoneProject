b0bbb138a6c6b73192e7a6fadbcafc1d5db88c05
alan-serrano <alan.serrano.pch@gmail.com>
2022-07-04 19:49:21 -0400

    feat/deliverable3 moving hooks

--- src/hooks/useFeaturedBanners.js
+++ src/hooks/useFeaturedBanners.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { API_BASE_URL } from '../constants';
+import { API_BASE_URL } from '../utils/constants';
 import { useLatestAPI } from './useLatestAPI';
 
 export function useFeaturedBanners() {
