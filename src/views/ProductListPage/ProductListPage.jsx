ae39d0413775e5968b281acf376be9f116cbbb39
alan-serrano <alan.serrano.pch@gmail.com>
2022-07-04 20:40:54 -0400

    feat/deliverable3 refactoring with fetched data

--- src/views/ProductListPage/ProductListPage.jsx
+++ src/views/ProductListPage/ProductListPage.jsx
@@ -3,38 +3,39 @@ import PropTypes from 'prop-types';
 import { FullLayout } from '../../layout';
 import { Styled } from './Sidebar';
 
-import productsJSON from '../../mocks/en-us/products.json';
 import ProductGrid from '../../components/ProductGrid';
 import { types } from '../../types';
-import { useProductCategories } from '../../hooks';
+import { useProductCategories, useProducts } from '../../hooks';
 
 const ProductListPage = (props) => {
   const { className } = props;
   const [{ results: productCategories }, categoriesAreLoading] =
     useProductCategories();
+  const [productsResponse, productsAreLoading] = useProducts();
   const [filters, setFilters] = useState([]);
   const [
     /**@type {types.productTypes.products}*/
     products,
     setProducts,
-  ] = useState(productsJSON.results);
+  ] = useState(productsResponse.results || []);
 
   useEffect(
     function filterProducts() {
+      if (productsAreLoading) return;
       setProducts((products) => {
         if (filters.length > 0) {
-          products = productsJSON.results.filter((product) => {
+          products = productsResponse.results.filter((product) => {
             let category = product.data.category;
             return filters.includes(category.id);
           });
         } else {
-          products = productsJSON.results;
+          products = productsResponse.results;
         }
 
         return products;
       });
     },
-    [filters],
+    [filters, productsAreLoading],
   );
 
   return (
