27d41c5ddad3df650dc2d425f0b90effbb7d7db4
alan-serrano <alan.serrano.pch@gmail.com>
2022-07-05 00:35:26 -0400

    feat/deliverable3 update useProduct to get id

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -2,12 +2,24 @@ import { useState, useEffect } from 'react';
 import { API_BASE_URL } from '../utils/constants';
 import { useLatestAPI } from './useLatestAPI';
 
-export function useProducts() {
+export function useProducts(props = {}) {
   const { ref: apiRef, isLoading: isApiMetadataLoading } = useLatestAPI();
+  const { id } = props;
   const [products, setProducts] = useState(() => ({
     data: {},
     isLoading: true,
   }));
+
+  let url = '';
+  if (id) {
+    url = `${API_BASE_URL}/documents/search?ref=${apiRef}&q=${encodeURIComponent(
+      `[[:d = at(document.id, "${id}") ]]`,
+    )}&lang=en-us&pageSize=12`;
+  } else {
+    url = `${API_BASE_URL}/documents/search?ref=${apiRef}&q=${encodeURIComponent(
+      '[[at(document.type, "product")]]',
+    )}&lang=en-us&pageSize=12`;
+  }
 
   useEffect(() => {
     if (!apiRef || isApiMetadataLoading) {
@@ -20,14 +32,9 @@ export function useProducts() {
       try {
         setProducts({ data: {}, isLoading: true });
 
-        const response = await fetch(
-          `${API_BASE_URL}/documents/search?ref=${apiRef}&q=${encodeURIComponent(
-            '[[at(document.type, "product")]]',
-          )}&lang=en-us&pageSize=12`,
-          {
-            signal: controller.signal,
-          },
-        );
+        const response = await fetch(url, {
+          signal: controller.signal,
+        });
         const data = await response.json();
 
         setProducts({ data, isLoading: false });
