b4462d5539baee9c8cdaba834c48b8b13693cbb6
alan-serrano <alan.serrano.pch@gmail.com>
2022-07-04 21:56:43 -0400

    feat/deliverable3 Refactoring

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { useState } from 'react';
 import './App.css';
 import { GlobalStyle } from './styles';
-import { useFeaturedBanners } from './utils/hooks/useFeaturedBanners';
 import { Outlet } from 'react-router-dom';
 
 function App() {
-  const { data, isLoading } = useFeaturedBanners();
-
   return (
     <div className="App">
       <GlobalStyle />
